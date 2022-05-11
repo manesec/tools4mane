f60ee3c6c9e0dee8b899bfdddffe48c8a678ef29
Mane <mane@manesec.com>
2022-05-11 17:18:21 +0800

    Create GitShowAllCommit.py

---
+++ Script/GitShowAllCommit.py
@@ -0,0 +1,22 @@
+import subprocess
+
+"""
+ ▄▀▀▄ ▄▀▄  ▄▀▀█▄   ▄▀▀▄ ▀▄  ▄▀▀█▄▄▄▄  ▄▀▀▀▀▄  ▄▀▀█▄▄▄▄  ▄▀▄▄▄▄  
+█  █ ▀  █ ▐ ▄▀ ▀▄ █  █ █ █ ▐  ▄▀   ▐ █ █   ▐ ▐  ▄▀   ▐ █ █    ▌ 
+▐  █    █   █▄▄▄█ ▐  █  ▀█   █▄▄▄▄▄     ▀▄     █▄▄▄▄▄  ▐ █      
+  █    █   ▄▀   █   █   █    █    ▌  ▀▄   █    █    ▌    █      
+▄▀   ▄▀   █   ▄▀  ▄▀   █    ▄▀▄▄▄▄    █▀▀▀    ▄▀▄▄▄▄    ▄▀▄▄▄▄▀ 
+█    █    ▐   ▐   █    ▐    █    ▐    ▐       █    ▐   █     ▐  
+▐    ▐            ▐         ▐                 ▐        ▐        
+                    Show all commit on git
+                           Version: 20220511
+                https://github.com/manesec/tools4me
+---------------------------------------------------------------
+"""
+
+gitlogs = subprocess.getoutput("git log | grep commit")
+for line in gitlogs.split("\n"):
+    line_sp = line.strip().split(" ")
+    if (line_sp[0] == "commit"):
+        print(subprocess.getoutput("git show %s" % (line_sp[1])))
+
