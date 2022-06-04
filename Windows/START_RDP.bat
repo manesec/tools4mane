abcc2c9f1d8d82cb115deba3f05923d39298f696
Mane <mane@manesec.com>
2022-06-04 15:51:58 +0800

    Update START_RDP.bat

--- Windows/START_RDP.bat
+++ Windows/START_RDP.bat
@@ -1,6 +1,7 @@
 rem test for XP, WIN7
 rem from https://www.cnblogs.com/lxy2015/p/4646714.html
 
+
 echo Windows Registry Editor Version 5.00>3389.reg
 echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server]>>3389.reg
 echo "fDenyTSConnections"=dword:00000000>>3389.reg
@@ -10,3 +11,5 @@ echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinSta
 echo "PortNumber"=dword:00000d3d>>3389.reg
 regedit /s 3389.reg
 del 3389.reg
+
+netsh advfirewall firewall add rule name="RDP 3389" dir=in action=allow protocol=TCP localport=3389
