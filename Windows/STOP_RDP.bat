ac34448b5a701ee750d04371311a46b54873af9e
Mane <mane@manesec.com>
2022-06-08 22:27:49 +0800

    update

--- Windows/STOP_RDP.bat
+++ Windows/STOP_RDP.bat
@@ -1,11 +1,3 @@
-rem from https://www.cnblogs.com/lxy2015/p/4646714.html
-
-echo Windows Registry Editor Version 5.00>3389.reg
-echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server]>>3389.reg
-echo "fDenyTSConnections"=dword:00000001>>3389.reg
-echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\Wds\rdpwd\Tds\tcp]>>3389.reg
-echo "PortNumber"=dword:00000d3d>>3389.reg
-echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp]>>3389.reg
-echo "PortNumber"=dword:00000d3d>>3389.reg
-regedit /s 3389.reg
-del 3389.reg
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\Wds\rdpwd\Tds\tcpr" /v PortNumber /t REG_DWORD /d 3389 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v PortNumber /t REG_DWORD /d 3389 /f
