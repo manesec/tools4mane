349b52d23edb78ff1b9e5b936714cf8ff014e066
Mane <mane@manesec.com>
2023-02-17 12:48:15 -0800

    Update setup_nvim.sh

--- Config/setup_nvim.sh
+++ Config/setup_nvim.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
-
+echo [*] apt updating ...
 sudo apt update
-sudo apt-get install python-dev python-pip python3-dev python3-pip vim
+
+echo [*] apt installing ...
+sudo apt-get install -y python-dev python-pip python3-dev python3-pip vim 
+
+echo [*] getting and setting neovim ...
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.deb -O /tmp/nvim-linux64.deb
 sudo dpkg -i /tmp/nvim-linux64.deb
 mkdir -p ~/.config/nvim
-sudo pip3 install debugpy
 wget https://raw.githubusercontent.com/manesec/tools4mane/main/Config/neovim/init.lua -O  ~/.config/nvim/init.lua
+
+echo [*] install python env ...
+sudo pip3 install debugpy
+sudo apt install -y python3-venv
