dcd627ad29c1a9c5e48e618305c69f7b5bb1331a
Mane <mane@manesec.com>
2023-02-17 12:43:04 -0800

    Update setup.sh

--- Config/setup.sh
+++ Config/setup.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 wget https://raw.githubusercontent.com/manesec/tools4mane/main/Config/tmux.conf -O ~/.tmux.conf
 wget https://raw.githubusercontent.com/manesec/tools4mane/main/Config/vimrc -O ~/.vimrc
-mkdir -p ~/.config/nvim
-wget https://raw.githubusercontent.com/manesec/tools4mane/main/Config/neovim/init.lua -O  ~/.config/nvim/init.lua
 echo Done.
