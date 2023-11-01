368229313b8209a39266f2ece37fbb94e16f063a
Mane <mane@manesec.com>
2023-11-02 00:01:55 +0800

    update zsh

--- Config/zsh/setup.sh
+++ Config/zsh/setup.sh
@@ -1,11 +1,35 @@
 #! /bin/bash
+echo "Zsh setup program, power by Tools4mane ..."
 
-echo "Zsh setup program, by Tools4mane ..."
+echo "[*] Checking curl "
+if ! command -v curl &> /dev/null
+then
+    echo "[*] curl could not be found, trying to use APT to install it."
+    sudo apt update && sudo apt install -y curl
+fi
+
+echo "[*] Clearning up zsh ..."
+rm -rf /home/mane/.oh-my-zsh 2> /dev/null
 
 echo "[*] Setting up zsh ..."
-mkdir -p "$HOME/.zsh"
-wget 
+sudo apt install -y zsh-autosuggestions zsh-syntax-highlighting zsh
 
-echo "[*] Installing Plugin ..."
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-mkdir -p "$HOME/"
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+
+echo "[*] Installing oh-my-zsh ..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+echo "[*] Install Plugin ..."
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+
+echo "[*] Making config ..."
+rm ~/.zshrc
+curl https://raw.githubusercontent.com/manesec/tools4mane/main/Config/zsh/zshrc -O ~/.zshrc
+
+echo "OK! Type 'zsh' to use zsh like bash!"
