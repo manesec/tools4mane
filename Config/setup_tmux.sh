cef11d27d3471a3f3f5851918886a5c774c55148
Mane <mane@manesec.com>
2024-03-20 14:23:20 +0800

    fix typo

--- Config/setup_tmux.sh
+++ Config/setup_tmux.sh
@@ -3,10 +3,10 @@ sudo apt install -y fzf jq
 mkdir -p ~/.tmux/plugins/
 
 echo "[Tmux] Cloning mane-tmux-macros ... "
-git clone https://github.com/manesec/tmux-command-macros.git ~/mane-tmux-macros
+git clone https://github.com/manesec/mane-tmux-macros ~/mane-tmux-macros
 
 echo "[Tmux] Cloning tmux-text-macros ... "
-git clone https://github.com/manesec/tmux-command-macros.git ~/.tmux/plugins/tmux-command-macro
+git clone https://github.com/manesec/tmux-command-macros.git ~/.tmux/plugins/tmux-command-macros
 
 echo "[Tmux] Installing config ... "
 wget https://raw.githubusercontent.com/manesec/tools4mane/main/Config/tmux/tmuxWithMacros.conf -O ~/.tmux.conf
