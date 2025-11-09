7bdb53596407cbe6cf452604aac5447a7154d647
Mane <mane@manesec.com>
2025-11-09 14:27:02 +0800

    Update Invoke-ProcessMonitor.ps1

--- Windows/Invoke-ProcessMonitor.ps1
+++ Windows/Invoke-ProcessMonitor.ps1
@@ -1,30 +1,85 @@
 # You can just copy and paste the code below in powershell
 
-$process = Get-WmiObject Win32_Process | Select-Object ProcessName, ExecutablePath, CommandLine;
 
-while($true)
-{
-  Start-Sleep 0.1;
-  $process2 = Get-WmiObject Win32_Process | Select-Object ProcessName, ExecutablePath, CommandLine;
-  $obj = Compare-Object -ReferenceObject $process -DifferenceObject $process2;
-  $process = $process2;
-  if ($obj) {
-	$ProcessName = $obj.InputObject.ProcessName;
-	$ExecutablePath = $obj.InputObject.ExecutablePath;
-	$CommandLine = $obj.InputObject.CommandLine;
-	
-	if (([string]::IsNullOrEmpty($CommandLine)))
+
+# Check Detection Method
+$detectMethod = 0;
+
+try {
+	$process = Get-WmiObject Win32_Process;
+	$detectMethod = 1;
+} catch { "[!] Permission Denied for WMI, Using Get-Process ..." }
+
+try {
+	$process = Get-Process;
+	$detectMethod = 2;
+} catch { "[!] Permission Denied for Get-Process, Giving up ..." }
+
+# Method 1: WMI
+if ($detectMethod -eq 1) {
+
+	$process = Get-WmiObject Win32_Process | Select-Object ProcessName, ProcessId, ExecutablePath, CommandLine;
+
+	while($true)
 	{
-		if (([string]::IsNullOrEmpty($ExecutablePath))){ 
-			$CommandLine = $ProcessName;
-		}else{
-			$CommandLine = $ExecutablePath ;
+		Start-Sleep 0.1;
+		$timestamp = Get-Date -Format "HH:mm:ss"; 
+		$process2 = Get-WmiObject Win32_Process | Select-Object ProcessName, ProcessId, ExecutablePath, CommandLine;
+		
+		$obj = Compare-Object -ReferenceObject $process -DifferenceObject $process2;
+		$process = $process2;
+		if ($obj) {
+			$ProcessName = $obj.InputObject.ProcessName;
+			$ExecutablePath = $obj.InputObject.ExecutablePath;
+			$CommandLine = $obj.InputObject.CommandLine;
+			$ProcessId = $obj.InputObject.ProcessId;
+			
+			if (([string]::IsNullOrEmpty($CommandLine)))
+			{
+				if (([string]::IsNullOrEmpty($ExecutablePath))){ 
+					$CommandLine = $ProcessName;
+				}else{
+					$CommandLine = $ExecutablePath ;
+				}
+			}
+			if ($obj.SideIndicator -eq "=>") { 
+				Write-Host "[$timestamp] STARTED: PID: $ProcessId - Path: $CommandLine" -ForegroundColor Green; 
+				
+			} else { 
+				Write-Host "[$timestamp] STOPPED: PID: $ProcessId - Path: $CommandLine" -ForegroundColor Red; 
+			}  
 		}
 	}
-	if ($obj.SideIndicator -eq "=>") { 
-		"[+] " + $CommandLine ;
-	} else { 
-		"[-] " + $CommandLine ;
-	}  
-  }
+}
+
+# Method 2: Get-Process
+if ($detectMethod -eq 2) {
+
+	$previousList = Get-Process | Select-Object -Property Id, Name, Path -ErrorAction SilentlyContinue;
+
+	while ($true) {
+		Start-Sleep -Seconds 0.1;
+		$currentList = Get-Process | Select-Object -Property Id, Name, Path -ErrorAction SilentlyContinue;
+		$differences = Compare-Object -ReferenceObject $previousList -DifferenceObject $currentList -Property Id -PassThru;
+		
+		if ($differences) {
+			$timestamp = Get-Date -Format "HH:mm:ss"; 
+			
+			foreach ($process in $differences) {
+				
+				$pathDisplay = $process.Path;
+				if ([string]::IsNullOrEmpty($pathDisplay)) {
+					$pathDisplay = "[Path not accessible]"; 
+				}
+
+				if ($process.SideIndicator -eq "=>") {
+					Write-Host "[$timestamp] STARTED: $($process.Name) (PID: $($process.Id)) - Path: $pathDisplay" -ForegroundColor Green; 
+				}
+				elseif ($process.SideIndicator -eq "<=") {
+					Write-Host "[$timestamp] STOPPED: $($process.Name) (PID: $($process.Id)) - Path: $pathDisplay" -ForegroundColor Red; 
+				}
+			}
+		}
+		$previousList = $currentList;
+	}
 }
